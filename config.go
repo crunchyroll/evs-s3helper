3171770661d57c29a34638056c60459cd23d7fa2
Christopher Kennedy <chris@ellation.com>
2022-03-28 14:34:57 -0700

    remove unused config items
    not used anymore, S3 AWS SDK handles this for us.

--- config.go
+++ config.go
@@ -1,7 +1,5 @@
 package main
 
-import "time"
-
 type nrConfig struct {
 	Name    string `yaml:"name"`
 	License string `yaml:"license"`
@@ -16,12 +14,10 @@ type logConfig struct {
 type Config struct {
 	Listen string `yaml:"listen"`
 
-	S3AdBucket string        `yaml:"s3_ad_bucket"`
-	S3Bucket   string        `yaml:"s3_bucket"`
-	S3Path     string        `yaml:"s3_prefix" optional:"true"`
-	S3Region   string        `yaml:"s3_region"`
-	S3Retries  int           `yaml:"s3_retries"`
-	S3Timeout  time.Duration `yaml:"s3_timeout"`
+	S3AdBucket string `yaml:"s3_ad_bucket"`
+	S3Bucket   string `yaml:"s3_bucket"`
+	S3Path     string `yaml:"s3_prefix" optional:"true"`
+	S3Region   string `yaml:"s3_region"`
 
 	Concurrency int       `yaml:"concurrency" optional:"true"`
 	Logging     logConfig `yaml:"logging"`
@@ -32,8 +28,6 @@ type Config struct {
 const defaultConfValues = `
     listen: "127.0.0.1:8080"
     concurrency: 0
-    s3_timeout:  120s
-    s3_retries:  10
     logging:
         ident: s3-helper
         level: "info"
