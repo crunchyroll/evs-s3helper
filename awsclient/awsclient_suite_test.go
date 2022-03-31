a71f14639d453bbdc70277ee1dbc9c47670d99e8
Christopher Kennedy <chris@ellation.com>
2022-03-31 13:07:29 -0700

    Revert "Revert "VOD-4148 Add NR to S3 Helper (trace, metrics, errors) + get errors from AWS failures (#34)""
    This reverts commit 08627e22ffa7d6c9e2c660765730fbef92f211b5.

---
+++ awsclient/awsclient_suite_test.go
@@ -0,0 +1,13 @@
+package awsclient_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestAwsclient(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Awsclient Suite")
+}
--- config.go
+++ config.go
@@ -1,26 +1,37 @@
 package main
 
-import "time"
+type nrConfig struct {
+	Name    string `yaml:"name"`
+	License string `yaml:"license"`
+}
+
+type logConfig struct {
+	Ident string `yaml:"ident"`
+	Level string `yaml:"level"`
+}
 
 // Config holds the global config
 type Config struct {
 	Listen string `yaml:"listen"`
 
-	S3AdBucket string        `yaml:"s3_ad_bucket"`
-	S3Bucket   string        `yaml:"s3_bucket"`
-	S3Path     string        `yaml:"s3_prefix" optional:"true"`
-	S3Region   string        `yaml:"s3_region"`
-	S3Retries  int           `yaml:"s3_retries"`
-	S3Timeout  time.Duration `yaml:"s3_timeout"`
+	S3AdBucket string `yaml:"s3_ad_bucket"`
+	S3Bucket   string `yaml:"s3_bucket"`
+	S3Path     string `yaml:"s3_prefix" optional:"true"`
+	S3Region   string `yaml:"s3_region"`
 
-	LogLevel    string `optional:"true"`
-	Concurrency int    `optional:"true"`
+	Concurrency int       `yaml:"concurrency" optional:"true"`
+	Logging     logConfig `yaml:"logging"`
+
+	NewRelic nrConfig `yaml:"newrelic"`
 }
 
 const defaultConfValues = `
     listen: "127.0.0.1:8080"
-    loglevel: "error"
-    s3_timeout:  5s
-    s3_retries:  5
-    concurrency:   0
+    concurrency: 0
+    logging:
+        ident: s3-helper
+        level: "info"
+    newrelic:
+        name: "proto0-s3-helper"
+        license: "None"
 `
