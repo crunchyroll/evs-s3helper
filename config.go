08627e22ffa7d6c9e2c660765730fbef92f211b5
Christopher Kennedy <chris@ellation.com>
2022-03-31 12:46:53 -0700

    Revert "VOD-4148 Add NR to S3 Helper (trace, metrics, errors) + get errors from AWS failures (#34)"
    This reverts commit 891c56c37439e67e4f91ecf9adae1459e9d29e52.

--- config.go
+++ config.go
@@ -1,37 +1,26 @@
 package main
 
-type nrConfig struct {
-	Name    string `yaml:"name"`
-	License string `yaml:"license"`
-}
-
-type logConfig struct {
-	Ident string `yaml:"ident"`
-	Level string `yaml:"level"`
-}
+import "time"
 
 // Config holds the global config
 type Config struct {
 	Listen string `yaml:"listen"`
 
-	S3AdBucket string `yaml:"s3_ad_bucket"`
-	S3Bucket   string `yaml:"s3_bucket"`
-	S3Path     string `yaml:"s3_prefix" optional:"true"`
-	S3Region   string `yaml:"s3_region"`
+	S3AdBucket string        `yaml:"s3_ad_bucket"`
+	S3Bucket   string        `yaml:"s3_bucket"`
+	S3Path     string        `yaml:"s3_prefix" optional:"true"`
+	S3Region   string        `yaml:"s3_region"`
+	S3Retries  int           `yaml:"s3_retries"`
+	S3Timeout  time.Duration `yaml:"s3_timeout"`
 
-	Concurrency int       `yaml:"concurrency" optional:"true"`
-	Logging     logConfig `yaml:"logging"`
-
-	NewRelic nrConfig `yaml:"newrelic"`
+	LogLevel    string `optional:"true"`
+	Concurrency int    `optional:"true"`
 }
 
 const defaultConfValues = `
     listen: "127.0.0.1:8080"
-    concurrency: 0
-    logging:
-        ident: s3-helper
-        level: "info"
-    newrelic:
-        name: "proto0-s3-helper"
-        license: "None"
+    loglevel: "error"
+    s3_timeout:  5s
+    s3_retries:  5
+    concurrency:   0
 `
--- main.go
+++ main.go
@@ -38,21 +38,21 @@ func main() {
 	}
 	log.Info().Msg(fmt.Sprintf("Loaded config from %s", *configFile))
 
-	if conf.Logging.Level == "error" {
+	if conf.LogLevel == "error" {
 		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
-	} else if conf.Logging.Level == "warn" {
+	} else if conf.LogLevel == "warn" {
 		zerolog.SetGlobalLevel(zerolog.WarnLevel)
-	} else if conf.Logging.Level == "info" {
+	} else if conf.LogLevel == "info" {
 		zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	} else if conf.Logging.Level == "debug" {
+	} else if conf.LogLevel == "debug" {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	} else if conf.Logging.Level == "panic" {
+	} else if conf.LogLevel == "panic" {
 		zerolog.SetGlobalLevel(zerolog.PanicLevel)
-	} else if conf.Logging.Level == "fatal" {
+	} else if conf.LogLevel == "fatal" {
 		zerolog.SetGlobalLevel(zerolog.FatalLevel)
 	} else {
 		zerolog.SetGlobalLevel(zerolog.WarnLevel)
-		log.Error().Msg(fmt.Sprintf("Bad loglevel given %s - defaulting to Warn level", conf.Logging.Level))
+		log.Error().Msg(fmt.Sprintf("Bad loglevel given %s - defaulting to Warn level", conf.LogLevel))
 	}
 
 	api := App{}
